18592c8f01f1ceaf8ba93f239d35d64edaf5b300
juan concha vallejos <jc@MacBook-Pro-de-juan-5.local>
2019-02-15 00:54:19 -0300

    Initial commit

---
+++ app/src/main/java/com/juanconcha/navegationdrawer/Activities/MainActivity.java
@@ -0,0 +1,157 @@
+package com.juanconcha.navegationdrawer.Activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.juanconcha.navegationdrawer.Fragments.AlertsFragment;
+import com.juanconcha.navegationdrawer.Fragments.EmailFragment;
+import com.juanconcha.navegationdrawer.Fragments.InfoFragment;
+import com.juanconcha.navegationdrawer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setToolbar();
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        navigationView= (NavigationView)findViewById(R.id.navview);
+
+        //metodo fragment por defecto
+        setFragmentByDefault();
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                Toast.makeText(MainActivity.this,"Open", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                Toast.makeText(MainActivity.this,"Close", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                //captura clicks en el group item
+
+                boolean fragmentTransaction = false;
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+
+                    case R.id.menu_mail:
+                        fragment = new EmailFragment();
+                        fragmentTransaction = true;
+
+                        break;
+
+                    case R.id.menu_alert:
+                        fragment = new AlertsFragment();
+                        fragmentTransaction = true;
+
+                        break;
+
+                    case R.id.menu_info:
+                        fragment = new InfoFragment();
+                        fragmentTransaction = true;
+
+                        break;
+
+                    case R.id.menu_opcion_uno:
+
+                        Toast.makeText(MainActivity.this,"Has clickeado en la opción 1", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case R.id.menu_opcion_dos:
+
+                        Toast.makeText(MainActivity.this,"Has clickeado en la opción 2", Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+                if(fragmentTransaction){
+                    changeFragment(fragment, item);
+                    drawerLayout.closeDrawers();
+
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //habilitar icono en la barra
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    private void setFragmentByDefault(){
+
+        changeFragment(new EmailFragment(), navigationView.getMenu().getItem(0));
+    }
+
+
+    private void changeFragment(Fragment fragment, MenuItem item){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+    }
+
+
+    //mostrar menu emergente
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                //abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
